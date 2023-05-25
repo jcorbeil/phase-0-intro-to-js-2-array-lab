fda7910002c5fd9f5ce6b1bda6c60ab4e65e2369
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-05-25 18:04:24 -0400

    finished Arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+const cats = ["Milo","Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}   
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+ const allCats = [...cats,name]
+ return allCats
+}
+function prependCat(name){
+    const copyOfCats = [name,...cats]
+    return copyOfCats
+}
+function removeLastCat(name){
+    const copyOfCats = [...cats]
+    return copyOfCats.slice(0,2)
+}
+function removeFirstCat(name){
+    const copyOfCats = [...cats]
+    return copyOfCats.slice(1,3)
+}
